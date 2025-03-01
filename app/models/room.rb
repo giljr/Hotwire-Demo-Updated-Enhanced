d89084c72f351f3a031fbe6885010cfe8c9fe713
jaythree <giljr.2009@gmail.com>
2025-03-01 16:39:43 -0400

    episodes 2: Streaming Over WebSockets - Turbo Streams

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,7 @@
 class Room < ApplicationRecord
     has_many :messages, dependent: :destroy
-    # validates :name, presence: true
-    # validates :name, uniqueness: true
-    # validates :name, length: { minimum: 3 }
-    # validates :name, length: { maximum: 20 }
-    # validates :name, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :name, length: { minimum: 3 }
+    validates :name, length: { maximum: 10 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
+  root 'rooms#index'
 end
